2e8cf739d62a8faf0bf3a3ce3be42782fb9c201d
Daniel Klock <danielkl@stud.ntnu.no>
2017-10-11 16:07:26 +0200

    Changed some CSS. Improved looks.

--- src/main/java/beans/Quiz.java
+++ src/main/java/beans/Quiz.java
@@ -4,7 +4,6 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.*;
 
 /**
